3da189209bc1f3bb02e5ee4c86c8d185389d4498
Sara Montagud <43246362+Aforina@users.noreply.github.com>
2024-02-13 16:20:52 +0100

    init: configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
-import { defineConfig } from 'astro/config';
+// astro.config.ts
+import { defineConfig } from 'astro/config'
+import UnoCSS from 'unocss/astro'
 
-import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
-});
+  integrations: [UnoCSS()]
+})
