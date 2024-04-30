cb539f79ef0eb505967f561bb325d50a7575ff17
Sara Montagud <43246362+Aforina@users.noreply.github.com>
2024-04-30 23:46:47 +0200

    feat: add lamp component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 // astro.config.ts
-import { defineConfig } from 'astro/config'
-import UnoCSS from 'unocss/astro'
-import vercel from '@astrojs/vercel/serverless'
+import { defineConfig } from 'astro/config';
+import UnoCSS from 'unocss/astro';
+import vercel from '@astrojs/vercel/serverless';
+import react from "@astrojs/react";
 
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [
-    UnoCSS({
-      injectReset: true
-    })
-  ],
+  integrations: [UnoCSS({
+    injectReset: true
+  }), react(), tailwind()],
   output: 'hybrid',
   adapter: vercel()
-})
+});
