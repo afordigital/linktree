6347f63b4ee3375095d96919e153705d2f0c2373
Sara Montagud <43246362+Aforina@users.noreply.github.com>
2024-05-18 20:35:08 +0200

    feat: fix borders, twitch presentation, new logos, new discord link

--- src/constants/Youtube.js
+++ src/constants/Youtube.js
@@ -4,7 +4,7 @@ const USER_ID = 'UCkDwFvETfXmnIwpX7lV3F1g'
 
 const FORMAT = '{"id":"{id}","title":"{title}"}'
 
-const QUERY_PARAMS = 'no_livestream=0&no_shorts=0&skip=1'
+const QUERY_PARAMS = 'no_livestream=0&no_shorts=0'
 
 const FINAL_URL = `${BASE_URL}id=${USER_ID}&format=${FORMAT}&${QUERY_PARAMS}`
 
