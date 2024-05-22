02fead118788e2d842784f75e61eb9b882fd9aa4
Sara Montagud <43246362+Aforina@users.noreply.github.com>
2024-05-22 23:37:19 +0200

    feat: ARROW TRANSPARENT and translations

---
+++ src/i18n/ui.ts
@@ -0,0 +1,27 @@
+export const languages = {
+  en: "English",
+  es: "Español",
+};
+
+export const defaultLang = "en";
+
+export const ui = {
+  en: {
+    "components.social.presentation":
+      "I am a front-end developer and I dedicate all my days to learn and spread what I learn.",
+    "components.title.twitch": "Follow me",
+    "components.title.portfolio": "My portfolio",
+    "components.title.portfolio.description":
+      "Know more about me and my projects",
+    "components.title.youtube": "My last video",
+  },
+  es: {
+    "components.social.presentation":
+      "Soy front-end developer y dedico todos mis días a aprender y divulgar lo que aprendo.",
+    "components.title.twitch": "Sígueme",
+    "components.title.portfolio": "Mi portfolio",
+    "components.title.portfolio.description":
+      "Conoce más sobre mí y mis proyectos",
+    "components.title.youtube": "Mi último video",
+  },
+} as const;
