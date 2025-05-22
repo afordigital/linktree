4306f63cc61d4e824e41ec2dbf9565a79162cb86
Sara Montagud <sara@know.ee>
2025-05-22 17:51:34 +0200

    fix: make links functional

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,27 +1,25 @@
 export const languages = {
-  en: "English",
-  es: "Español",
-};
+	en: 'English',
+	es: 'Español',
+}
 
-export const defaultLang = "en";
+export const defaultLang = 'en'
 
 export const ui = {
-  en: {
-    "components.social.presentation":
-      "I am a front-end developer and I dedicate all my days to learn and spread what I learn.",
-    "components.title.twitch": "Follow me",
-    "components.title.portfolio": "My portfolio",
-    "components.title.portfolio.description":
-      "Know more about me and my projects",
-    "components.title.youtube": "My last video",
-  },
-  es: {
-    "components.social.presentation":
-      "Soy front-end developer y dedico todos mis días a aprender y divulgar lo que aprendo.",
-    "components.title.twitch": "Sígueme",
-    "components.title.portfolio": "Mi portfolio",
-    "components.title.portfolio.description":
-      "Conoce más sobre mí y mis proyectos",
-    "components.title.youtube": "Mi último video",
-  },
-} as const;
+	en: {
+		'components.social.presentation':
+			'Product engineer that dedicates all her days to learn and spread what she learns.',
+		'components.title.twitch': 'Follow me',
+		'components.title.portfolio': 'My portfolio',
+		'components.title.portfolio.description': 'Know more about me and my projects',
+		'components.title.youtube': 'My last video',
+	},
+	es: {
+		'components.social.presentation':
+			'Ingeniera de producto que dedico todos sus días a aprender y divulgar lo que aprende.',
+		'components.title.twitch': 'Sígueme',
+		'components.title.portfolio': 'Mi portfolio',
+		'components.title.portfolio.description': 'Conoce más sobre mí y mis proyectos',
+		'components.title.youtube': 'Mi último video',
+	},
+} as const
